7246756a06cfd447bef68917007beb07a6938b23
Apsapeh <timofeykirichenko@gmail.com>
2023-08-19 02:28:22 +0300

    Added light

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,4 @@
-add_requires("glfw")
+add_requires("glfw", "glm")
 
 target("Old3DEngine")
     set_kind("binary")
@@ -7,4 +7,4 @@ target("Old3DEngine")
     add_files("src/*.cpp", "src/Render/*.cpp", "src/Utilities/*.cpp", "src/Objects/*.cpp", "src/Objects/Light/*.cpp", "src/*.c")
     set_languages("cxx11")
     add_includedirs("include")
-    add_packages("glfw")
+    add_packages("glfw", "glm")
