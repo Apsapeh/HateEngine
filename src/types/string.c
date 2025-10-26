97b9e50858f4a565a017e0a841a496848218a0af
= <venikov2011@yandex.ru>
2025-10-26 20:39:04 +0300

    intermedia stage

--- src/types/string.c
+++ src/types/string.c
@@ -1,197 +1,218 @@
-#include "string.h"
-#include <string.h>
+#include "String.h"
+#include <String.h>
 #include "error.h"
 #include "types.h"
 #include "../platform/memory.h"
 #include "../log.h"
 
-Error string_new(string** str) {
-    ERROR_ARG_CHECK(str, { return ERROR_INVALID_ARGUMENT; });
-
-    string* str_new = tmalloc(sizeof(string));
-    ERROR_ARG_CHECK(str_new, { return ERROR_INVALID_ARGUMENT; });
+String* string_new(void) {
+    String* str_new = tmalloc(sizeof(String));
+    ERROR_ALLOC_CHECK(str_new, { return NULL; });
 
     str_new->ptr = tmalloc(sizeof(u8));
-    ERROR_ARG_CHECK(str_new->ptr, { return ERROR_INVALID_ARGUMENT; });
+    ERROR_ALLOC_CHECK(str_new, { return NULL; });
 
     *(str_new->ptr) = '\0';
     str_new->len = 0;
-    *str = str_new;
 
-    return ERROR_SUCCESS;
-}
-
-string* string_from(const char* c_str) {
-    if (!c_str)
-        return NULL;
-
-    string* str = tmalloc(sizeof(string));
-    if (!str)
-        return NULL;
-
-    usize c_str_len = strlen(c_str);
-    str->ptr = tmalloc(c_str_len + 1);
-    str->len = c_str_len;
-    if (!str->ptr) {
-        tfree(str);
-        return NULL;
-    }
-
-    memcpy(str->ptr, c_str, c_str_len + 1);
-    return str;
-}
-
-string* string_clone(const string* c_str) {
-    if (!c_str)
-        return NULL;
-
-    string* str_new = tmalloc(sizeof(string));
-    if (!c_str)
-        return NULL;
-
-    str_new->ptr = tmalloc(c_str->len + 1);
-    if (!str_new->ptr) {
-        tfree(str_new);
-        return NULL;
-    }
-
-    str_new->len = c_str->len;
-    memcpy(str_new->ptr, c_str->ptr, c_str->len + 1);
     return str_new;
 }
 
-string* string_set_cstr(string* self, const char* c_cstr) {
-    usize c_cstr_len = strlen(c_cstr);
+String* string_from(const char* c_str) {
+    ERROR_ARG_CHECK(c_str, { return NULL; });
 
-    u8* tmp_ptr = trealloc(self->ptr, c_cstr_len + 1);
-    if (!tmp_ptr)
-        return NULL;
+    String* str = tmalloc(sizeof(String));
+    ERROR_ALLOC_CHECK(str, { return NULL; });
+
+    usize c_str_len = strlen(c_str);
+    str->ptr = tmalloc(c_str_len + 1);
+    ERROR_ALLOC_CHECK(str->ptr, { 
+        tfree(str);
+        return NULL; 
+    });
+
+    str->len = c_str_len;
+    memcpy(str->ptr, c_str, c_str_len + 1);
+
+    return str;
+}
+
+String* string_clone(const String* str) {
+    ERROR_ARG_CHECK(str, { return NULL; });
+
+    String* str_new = tmalloc(sizeof(String));
+    ERROR_ALLOC_CHECK(str_new, { return NULL; });
+
+    str_new->ptr = tmalloc(str->len + 1);
+    ERROR_ALLOC_CHECK(str_new->ptr, { 
+        tfree(str_new);
+        return NULL; 
+    });
+
+    str_new->len = str->len;
+    memcpy(str_new->ptr, str->ptr, str->len + 1);
+
+    return str_new;
+}
+
+String* string_set_cstr_len(String* self, const char* c_str, const usize len_c_str) {
+    ERROR_ARGS_CHECK_2(self, c_str, { return NULL; });
+
+    u8* tmp_ptr = trealloc(self->ptr, len_c_str + 1);
+    ERROR_ALLOC_CHECK(tmp_ptr, { return NULL; });
 
     self->ptr = tmp_ptr;
-    self->len = c_cstr_len;
-    memcpy(self->ptr, c_cstr, c_cstr_len + 1);
+    self->len = len_c_str;
+    memcpy(self->ptr, c_str, len_c_str + 1);
+
     return self;
 }
 
-string* string_set(string* self, const string* c_str) {
-    return string_set_cstr(self, (char*) c_str->ptr);
+String* string_set_cstr(String* self, const char* c_str) {
+    return string_set_cstr_len(self, c_str, strlen(c_str));
 }
 
-char* string_cstr(const string* c_str) {
-    if (!c_str)
-        return NULL;
-
-    return (char*) c_str->ptr;
+String* string_set(String* self, const String* str) {
+    return string_set_cstr_len(self, (char*) str->ptr, str->len);
 }
 
-usize string_len(const string* c_str) {
-    if (!c_str)
-        return -1;
+char* string_cstr(const String* self) {
+    ERROR_ARG_CHECK(self, { return NULL; });
+    return (char*) self->ptr;
+}
 
+usize string_len(const String* c_str) {
     return c_str->len;
 }
 
-usize string_size(const string* c_str) {
+usize string_size(const String* c_str) {
     return string_len(c_str) + 1;
 }
 
-string* string_push_back(string* dest, const string* src) {
-    if (!src)
-        return NULL;
-
-    return string_push_back_cstr(dest, (char*) src->ptr);
+String* string_push_back(String* self, const String* src) {
+    ERROR_ARGS_CHECK_2(self, src, { return NULL; });
+    return string_push_back_cstr_ex(self, (char*) src->ptr);
 }
 
-string* string_push_back_cstr(string* dest, const char* src) {
-    if (!dest || !src)
-        return NULL;
+String* string_push_back_cstr(String* self, const char* src) {
+    ERROR_ARGS_CHECK_2(self, src, { return NULL; });
+    return string_push_back_cstr_ex(self, src);
+}
 
+String* string_push_back_cstr_ex(String* self, const char* src) {
     usize len = strlen(src);
-    u8* tmp_ptr = trealloc(dest->ptr, dest->len + len + 1);
-    if (!tmp_ptr)
-        return NULL;
+    u8* tmp_ptr = trealloc(self->ptr, self->len + len + 1);
+    ERROR_ALLOC_CHECK(tmp_ptr, { return NULL; });
 
-    dest->ptr = tmp_ptr;
-    memcpy(dest->ptr + dest->len, src, len + 1);
-    dest->len += len;
+    self->ptr = tmp_ptr;
+    memcpy(self->ptr + self->len, src, len + 1);
+    self->len += len;
 
-    return dest;
+    return self;
 }
 
-string* string_push_front(string* dest, const string* src) {
-    if (!src)
-        return NULL;
-
-    return string_push_front_cstr(dest, (char*) src->ptr);
+String* string_push_front(String* self, const String* src) {
+    ERROR_ARGS_CHECK_2(self, src, { return NULL; });
+    return string_push_front_cstr(self, (char*) src->ptr);
 }
 
-string* string_push_front_cstr(string* dest, const char* src) {
-    if (!dest || !src)
-        return NULL;
+String* string_push_front_cstr(String* self, const char* src) {
+    ERROR_ARGS_CHECK_2(self, src, { return NULL; });
+    return string_push_front_cstr_ex(self, src);
+}
 
+String* string_push_front_cstr_ex(String* self, const char* src) {
     usize len = strlen(src);
-    u8* tmp_ptr = trealloc(dest->ptr, dest->len + len + 1);
-    if (!tmp_ptr)
+    u8* tmp_ptr = trealloc(self->ptr, self->len + len + 1);
+    ERROR_ALLOC_CHECK(tmp_ptr, { return NULL; });
+
+    self->ptr = tmp_ptr;
+    memcpy(self->ptr + len, self->ptr, self->len + 1);
+    memcpy(self->ptr, src, len);
+    self->len += len;
+
+    return self;
+}
+
+String* string_insert(String* self, const String* src, const usize i) {
+    ERROR_ARGS_CHECK_2(self, src, { return NULL; });
+
+    usize len = string_len(self);
+    if (i < 0 || i >= len) {
+        LOG_ERROR_OR_DEBUG_FATAL("Invalid argument (output for boundary): i");
+        set_error(ERROR_INVALID_ARGUMENT);
         return NULL;
+    }
 
-    dest->ptr = tmp_ptr;
-    memcpy(dest->ptr + len, dest->ptr, dest->len + 1);
-    memcpy(dest->ptr, src, len);
-    dest->len += len;
-
-    return dest;
+    return string_insert_cstr_ex(self, (char*) src->ptr, i, strlen((char*) src->ptr));
 }
 
-string* string_insert(string* dest, const string* src, const usize i) {
-    return string_insert_cstr(dest, (char*) src->ptr, i);
-}
+String* string_insert_cstr(String* self, const char* src, const usize i) {
+    ERROR_ARGS_CHECK_2(self, src, { return NULL; });
 
-string* string_insert_cstr(string* dest, const char* src, const usize i) {
-    usize len = string_len(dest);
-    if (len == -1 || i < 0 || i >= len || !src)
+    usize len = string_len(self);
+    if (i < 0 || i >= len) {
+        LOG_ERROR_OR_DEBUG_FATAL("Invalid argument (output for boundary): i");
+        set_error(ERROR_INVALID_ARGUMENT);
         return NULL;
+    }
 
-    return string_insert_cstr_ex(dest, src, i);
+    return string_insert_cstr_ex(self, src, i, len);
 }
 
-string* string_insert_cstr_by_byte(string* dest, const char* src, const usize b) {
-    usize len = string_len(dest);
-    if (len == -1 || b < 0 || b > len || !src)
+String* string_insert_cstr_by_byte(String* self, const char* src, const usize b) {
+    ERROR_ARGS_CHECK_2(self, src, { return NULL; });
+
+    usize len = string_len(self);
+    if (b < 0 || b > len) {
+        LOG_ERROR_OR_DEBUG_FATAL("Invalid argument (output for boundary): i");
+        set_error(ERROR_INVALID_ARGUMENT);
         return NULL;
+    }
 
-    return string_insert_cstr_ex(dest, src, b);
+    return string_insert_cstr_ex(self, src, b, len);
 }
 
-string* string_insert_cstr_ex(string* dest, const char* src, const usize i) {
+String* string_insert_cstr_ex(String* self, const char* src, const usize i, const usize len_src) {
     if (i == 0)
-        return string_push_front_cstr(dest, src);
+        return string_push_front_cstr(self, src);
 
-    usize len = dest->len;
-    usize len_src = strlen(src);
-    u8* tmp_ptr = trealloc(dest->ptr, dest->len + len_src + 1);
+    usize len = self->len;
+    u8* tmp_ptr = trealloc(self->ptr, self->len + len_src + 1);
+    ERROR_ALLOC_CHECK(tmp_ptr, { return NULL; });
 
-    if (!tmp_ptr)
-        return NULL;
+    self->ptr = tmp_ptr;
+    memcpy(self->ptr + len_src + i, self->ptr + i, self->len - i + 1);
+    memcpy(self->ptr + i, src, len_src);
+    self->len += len_src;
 
-    dest->ptr = tmp_ptr;
-    memcpy(dest->ptr + len_src + i, dest->ptr + i, dest->len - i + 1);
-    memcpy(dest->ptr + i, src, len_src);
-    dest->len += len_src;
-
-    return dest;
+    return self;
 }
 
-string* string_insert_by_byte(string* dest, const string* src, const usize b) {
-    return string_insert_cstr_by_byte(dest, (char*) src->ptr, b);
+String* string_insert_by_byte(String* self, const String* src, const usize b) {
+    ERROR_ARGS_CHECK_2(self, src, { return NULL; });
+
+    usize len = string_len(self);
+    if (b < 0 || b > len) {
+        LOG_ERROR_OR_DEBUG_FATAL("Invalid argument (output for boundary): i");
+        set_error(ERROR_INVALID_ARGUMENT);
+        return NULL;
+    }
+
+    return string_insert_cstr_ex(self, (char*) src->ptr, b, len);
 }
 
-string* string_remove(string* self, const usize i) {
-    if (!self || i < 0 || i >= self->len)
+String* string_remove(String* self, const usize i) {
+    ERROR_ARG_CHECK(self, { return NULL; });
+
+    if (i < 0 || i >= self->len) {
+        LOG_ERROR_OR_DEBUG_FATAL("Invalid argument (output for boundary): i");
+        set_error(ERROR_INVALID_ARGUMENT);
         return NULL;
+    }
 
     u8* tmp_ptr = trealloc(self->ptr, self->len);
-    if (!tmp_ptr)
-        return NULL;
+    ERROR_ALLOC_CHECK(tmp_ptr, { return NULL; });
 
     memcpy(self->ptr + i, self->ptr + i + 1, self->len - i);
     self->len -= 1;
@@ -200,18 +221,24 @@ string* string_remove(string* self, const usize i) {
     return self;
 }
 
-string* string_remove_by_byte(string* self, const usize b) {
+String* string_remove_by_byte(String* self, const usize b) {
     return string_remove(self, b);
 }
 
-string* string_remove_n(string* self, const usize i, const usize n) {
+String* string_remove_n(String* self, const usize i, const usize n) {
     if (n == 0)
         return string_remove(self, i);
-    if (!self || i < 0 || i >= self->len || n < 0 || i + n >= self->len)
+
+    ERROR_ARG_CHECK(self, { return NULL; });
+
+    if (i < 0 || i >= self->len || n < 0 || i + n >= self->len) {
+        LOG_ERROR_OR_DEBUG_FATAL("Invalid argument (output for boundary): i");
+        set_error(ERROR_INVALID_ARGUMENT);
         return NULL;
+    }
 
     u8* tmp_ptr;
-
+    
     if (i + n == self->len) {
         u8* tmp_ptr = trealloc(self->ptr, i);
         if (!tmp_ptr)
@@ -234,30 +261,30 @@ string* string_remove_n(string* self, const usize i, const usize n) {
     return self;
 }
 
-string* string_remove_n_by_byte(string* self, const usize b, const usize b_n) {
+String* string_remove_n_by_byte(String* self, const usize b, const usize b_n) {
     return string_remove_n(self, b, b_n);
 }
 
-boolean string_equals(const string* str1, const string* str2) {
+boolean string_equals(const String* str1, const String* str2) {
     if (!str1 || !str2 || str1->len != str2->len)
         return false;
 
     return strcmp(string_cstr(str1), string_cstr(str2)) ? false : true;
 }
 
-boolean string_equals_cstr(const string* str, const char* cstr) {
+boolean string_equals_cstr(const String* str, const char* cstr) {
     if (!str || !cstr || str->len != strlen(cstr))
         return false;
 
     return strcmp(string_cstr(str), cstr) ? false : true;
 }
 
-void string_free(string* c_pstr) {
+void string_free(String* c_pstr) {
     tfree(c_pstr->ptr);
     tfree(c_pstr);
 }
 
-string_itr* string_get_itr(const string* c_str) {
+string_itr* string_get_itr(const String* c_str) {
     string_itr* str_itr = tmalloc(sizeof(string_itr));
     if (!str_itr)
         return NULL;
@@ -277,7 +304,7 @@ void string_itr_free(string_itr* str_itr) {
     tfree(str_itr);
 }
 
-string_slice* string_get_slice(const string* str, const usize s, const usize e) {
+string_slice* string_get_slice(const String* str, const usize s, const usize e) {
     if (!str || s < 0 || s >= str->len || e < s || e >= str->len)
         return NULL;
 
@@ -297,11 +324,11 @@ string_slice* string_get_slice(const string* str, const usize s, const usize e) 
     return str_sl;
 }
 
-string* string_from_slice(const string_slice* str_sl) {
+String* string_from_slice(const string_slice* str_sl) {
     if (!str_sl)
         return NULL;
 
-    string* str = tmalloc(sizeof(string));
+    String* str = tmalloc(sizeof(String));
     if (!str)
         return NULL;
 
@@ -335,7 +362,7 @@ string_slice* string_set_slice(string_slice* self, const string_slice* str_sl) {
     return self;
 }
 
-string* string_push_back_slice(string* dest, const string_slice* src) {
+String* string_push_back_slice(String* dest, const string_slice* src) {
     if (!dest || !src)
         return NULL;
 
@@ -350,7 +377,7 @@ string* string_push_back_slice(string* dest, const string_slice* src) {
     return dest;
 }
 
-string* string_push_front_slice(string* dest, const string_slice* src) {
+String* string_push_front_slice(String* dest, const string_slice* src) {
     if (!dest || !src)
         return NULL;
 
@@ -366,7 +393,7 @@ string* string_push_front_slice(string* dest, const string_slice* src) {
     return dest;
 }
 
-string* string_insert_slice_ex(string* dest, const string_slice* src, const usize i) {
+String* string_insert_slice_ex(String* dest, const string_slice* src, const usize i) {
     if (i == 0)
         return string_push_front_slice(dest, src);
 
@@ -382,7 +409,7 @@ string* string_insert_slice_ex(string* dest, const string_slice* src, const usiz
     return dest;
 }
 
-string* string_insert_slice_by_byte(string* dest, const string_slice* src, const usize b) {
+String* string_insert_slice_by_byte(String* dest, const string_slice* src, const usize b) {
     usize len = string_len(dest);
     if (len == -1 || b < 0 || b > len || !src)
         return NULL;
@@ -391,7 +418,7 @@ string* string_insert_slice_by_byte(string* dest, const string_slice* src, const
 }
 
 
-string* string_insert_slice(string* dest, const string_slice* src, const usize i) {
+String* string_insert_slice(String* dest, const string_slice* src, const usize i) {
     usize len = string_len(dest);
     if (len == -1 || i < 0 || i >= len || !src)
         return NULL;
@@ -464,7 +491,7 @@ static Error string_utf8_dec(string_utf8** str, const u8* c_str) {
     return ERROR_SUCCESS;
 }
 
-Error string_utf8_to_string(string** dest, const string_utf8* str) {
+Error string_utf8_to_string(String** dest, const string_utf8* str) {
     ERROR_ARGS_CHECK_2(dest, str, { return ERROR_INVALID_ARGUMENT; });
 
     usize len_str;
@@ -493,7 +520,7 @@ Error string_utf8_to_string(string** dest, const string_utf8* str) {
     }
     *tmp_ptr = '\0';
 
-    string* str_new = tmalloc(sizeof(string));
+    String* str_new = tmalloc(sizeof(String));
     ERROR_ARG_CHECK(str_new, { return ERROR_INVALID_ARGUMENT; });
 
     usize size_new_str = tmp_ptr - cache + 1;
@@ -528,7 +555,7 @@ Error string_utf8_size(usize* size, const string_utf8* c_str) {
 Error string_utf8_clone(string_utf8** str, const string_utf8* c_str) {
     ERROR_ARGS_CHECK_2(str, c_str, { return ERROR_INVALID_ARGUMENT; });
 
-    string_utf8* str_new = tmalloc(sizeof(string));
+    string_utf8* str_new = tmalloc(sizeof(String));
     ERROR_ARG_CHECK(str_new, { return ERROR_INVALID_ARGUMENT; });
 
     usize size_c_str = 4 * c_str->len + 1;
--- src/types/string.h
+++ src/types/string.h
@@ -10,7 +10,7 @@
 typedef struct {
     u8* ptr;
     usize len;
-} string;
+} String;
 
 /**
  * @api
@@ -31,141 +31,174 @@ typedef struct {
 
 /**
  * @api
+ * @brief create empty String with null-terminator
  */
-Error string_new(string** str);
+String* string_new(void);
 
 /**
  * @api
+ * @brief create String by char*
  */
-string* string_from(const char* c_str);
+String* string_from(const char* c_str);
 
 /**
  * @api
+ * @brief clone String by existing String
  */
-string* string_clone(const string* c_str);
+String* string_clone(const String* c_str);
 
 /**
  * @api
+ * @brief update String by char* knowing its len
+ * @param c_str pointer on new content of String
+ * @param len_c_str len of c_str
  */
-string* string_set_cstr(string* self, const char* c_cstr);
+String* string_set_cstr_len(String* self, const char* c_str, const usize len_c_str);
 
 /**
  * @api
+ * @brief update String by char*
+ * @param c_str pointer on new content of String
  */
-string* string_set(string* self, const string* c_str);
+String* string_set_cstr(String* self, const char* c_str);
+
+/**
+ * @api
+ * @brief update String by existing String
+ * @param str  copied String
+ */
+String* string_set(String* self, const String* str);
+
+/**
+ * @api
+ * @brief get String by way char*
+ */
+char* string_cstr(const String* self);
+
+/**
+ * @api
+ * @brief get len of the passed String
+ */
+usize string_len(const String* c_str);
+
+/**
+ * @api
+ * @brief get size of the passed String
+ */
+usize string_size(const String* c_str);
+
+/**
+ * @api
+ * @brief add src-String in the end of self-String
+ * @param src  String that need to add
+ */
+String* string_push_back(String* self, const String* src);
+
+/**
+ * @api
+ * @brief add  char* in the end of self-String
+ * @param src  char* that need to add
+ */
+String* string_push_back_cstr(String* self, const char* src);
+
+String* string_push_back_cstr_ex(String* self, const char* src);
+
+/**
+ * @api
+ * @brief add  src-String in the front of self-String
+ * @param src  String that need to add
+ */
+String* string_push_front(String* self, const String* src);
+
+/**
+ * @api
+ * @brief add  char* in the front of self-String
+ * @param src  char* that need to add
+ */
+String* string_push_front_cstr(String* self, const char* src);
+
+String* string_push_front_cstr_ex(String* self, const char* src);
+
+/**
+ * @api
+ * @brief insert src-String in self-String by symbol index
+ * @param src String that need to insert
+ * @param i   symbol index
+ */
+String* string_insert(String* self, const String* src, const usize i);
+
+/**
+ * @api
+ * @brief insert char* in self-String by symbol index
+ * @param src char* that need to insert
+ * @param i   symbol index
+ */
+String* string_insert_cstr(String* self, const char* src, const usize i);
+
+String* string_insert_cstr_ex(String* self, const char* src, const usize i, const usize len_src);
+
+/**
+ * @api
+ * @brief insert char* in self-String by byte index
+ * @param src char* that need to insert
+ * @param b   byte index
+ */
+String* string_insert_cstr_by_byte(String* self, const char* src, const usize b);
+
+/**
+ * @api
+ * @brief insert src-String in self-String by byte index
+ * @param src String that need to insert
+ * @param b   byte index
+ */
+String* string_insert_by_byte(String* self, const String* src, const usize b);
+
+/**
+ * @api
+ * @brief remove symbol under symbol index
+ * @param i symbol index
+ */
+String* string_remove(String* self, const usize i);
+
+/**
+ * @api
+ * @brief remove symbol under byte index
+ * @param b byte index
+ */
+String* string_remove_by_byte(String* self, const usize b);
+
+/**
+ * @api
+ * @brief remove n of symbols since symbol  under symbol index
+ * @param i symbol index
+ * @param n count removed symbols
+ */
+String* string_remove_n(String* self, const usize i, const usize n);
+
+/**
+ * @api
+ * @brief remove n of byte since byte  under byte index
+ * @param b byte index
+ * @param b_n count removed byte
+ */
+String* string_remove_n_by_byte(String* self, const usize b, const usize b_n);
 
 /**
  * @api
  */
 // test
-char* string_cstr(const string* c_str);
+boolean string_equals(const String* str1, const String* str2);
 
 /**
  * @api
  */
 // test
-usize string_len(const string* c_str);
+boolean string_equals_cstr(const String* str, const char* cstr);
 
 /**
  * @api
  */
-// test
-usize string_size(const string* c_str);
-
-/**
- * @api
- */
-// test
-string* string_push_back(string* dest, const string* src);
-
-/**
- * @api
- */
-// test
-string* string_push_back_cstr(string* dest, const char* src);
-
-/**
- * @api
- */
-// test
-string* string_push_front(string* dest, const string* src);
-
-/**
- * @api
- */
-// test
-string* string_push_front_cstr(string* dest, const char* src);
-
-/**
- * @api
- */
-// test
-string* string_insert(string* dest, const string* src, const usize i);
-
-/**
- * @api
- */
-// test
-string* string_insert_cstr(string* dest, const char* src, const usize i);
-
-/**
- * @api
- */
-// test
-string* string_insert_cstr_ex(string* dest, const char* src, const usize i);
-
-/**
- * @api
- */
-// test
-string* string_insert_cstr_by_byte(string* dest, const char* src, const usize b);
-
-/**
- * @api
- */
-// test
-string* string_insert_by_byte(string* dest, const string* src, const usize b);
-
-/**
- * @api
- */
-// test
-string* string_remove(string* self, const usize i);
-
-/**
- * @api
- */
-// test
-string* string_remove_by_byte(string* self, const usize b);
-
-/**
- * @api
- */
-// test
-string* string_remove_n(string* self, const usize i, const usize n);
-
-/**
- * @api
- */
-// test
-string* string_remove_n_by_byte(string* self, const usize b, const usize b_n);
-
-/**
- * @api
- */
-// test
-boolean string_equals(const string* str1, const string* str2);
-
-/**
- * @api
- */
-// test
-boolean string_equals_cstr(const string* str, const char* cstr);
-
-/**
- * @api
- */
-void string_free(string* str);
+void string_free(String* str);
 
 /**
  * @api
@@ -175,7 +208,7 @@ void string_itr_free(string_itr* str_itr);
 /**
  * @api
  */
-string_itr* string_get_itr(const string* c_str);
+string_itr* string_get_itr(const String* c_str);
 
 /**
  * @api
@@ -185,12 +218,12 @@ u8 string_itr_next(string_itr* str_itr);
 /**
  * @api
  */
-string_slice* string_get_slice(const string* str, const usize s, const usize e);
+string_slice* string_get_slice(const String* str, const usize s, const usize e);
 
 /**
  * @api
  */
-string* string_from_slice(const string_slice* str_sl);
+String* string_from_slice(const string_slice* str_sl);
 
 /**
  * @api
@@ -206,31 +239,31 @@ string_slice* string_set_slice(string_slice* self, const string_slice* str_sl);
  * @api
  */
 // test
-string* string_push_back_slice(string* dest, const string_slice* src);
+String* string_push_back_slice(String* dest, const string_slice* src);
 
 /**
  * @api
  */
 // test
-string* string_push_front_slice(string* dest, const string_slice* src);
+String* string_push_front_slice(String* dest, const string_slice* src);
 
 /**
  * @api
  */
 // test
-string* string_insert_slice_ex(string* dest, const string_slice* src, const usize i);
+String* string_insert_slice_ex(String* dest, const string_slice* src, const usize i);
 
 /**
  * @api
  */
 // test
-string* string_insert_slice(string* dest, const string_slice* src, const usize i);
+String* string_insert_slice(String* dest, const string_slice* src, const usize i);
 
 /**
  * @api
  */
 // test
-string* string_insert_slice_by_byte(string* dest, const string_slice* src, const usize b);
+String* string_insert_slice_by_byte(String* dest, const string_slice* src, const usize b);
 
 /**
  * @api
@@ -265,7 +298,7 @@ Error string_utf8_new(string_utf8** str);
 /**
  * @api
  */
-Error string_utf8_to_string(string** dest, const string_utf8* str);
+Error string_utf8_to_string(String** dest, const string_utf8* str);
 
 /**
  * @api
